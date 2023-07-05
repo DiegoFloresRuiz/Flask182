3ee24ad03936693bcdc0a87304e950432b176fa9
DiegoFloresRuiz <133287417+DiegoFloresRuiz@users.noreply.github.com>
2023-07-04 18:55:04 -0600

    Practica8:EditarAlbumCompleta

--- Practica 5/App.py
+++ Practica 5/App.py
@@ -48,6 +48,33 @@ def guardar():
 def eliminar(): 
     return "Se elimino el album de la BD"
 
+
+@app.route('/editar/<id>')
+def editar(id):
+    curEditar = mysql.connection.cursor()
+    curEditar.execute('select * from albums where id = %s ', (id,))
+    consulId= curEditar.fetchone()
+    return render_template('EditarAlbum.html', album= consulId)
+
+
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+    if request.method == 'POST':
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vanio = request.form['txtAnio']
+
+        curAct= mysql.connection.cursor()
+        curAct.execute('update albums set titulo = %s, artista = %s, anio = %s where id = %s', (Vtitulo, Vartista, Vanio, id))
+        mysql.connection.commit()
+
+    flash('Album Actualizado en la BD') #manda un mensaje junto con la vista 
+    return redirect(url_for('index'))
+
+
+
+
+
 # Ejecutan el servidor 
 if __name__=='__main__':
     app.run(port= 5000, debug=True) #debug=true activaactualizacion 
